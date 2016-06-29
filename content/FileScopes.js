2a30248ed4c3a16441f445c52fe5525e062b5a40
babobski <info@babobski.nl>
2016-06-29 23:20:47 +0200

    forgot one sanitize check

--- content/FileScopes.js
+++ content/FileScopes.js
@@ -12,7 +12,7 @@ if (typeof(extensions.sass) === 'undefined') extensions.sass = {
 		overlay = self.overlay;
 
 
-	if (!('sass' in ko)) ko.extensions = {};
+	if (!('extensions' in ko)) ko.extensions = {};
 	var myExt = "SassCompiler@komodoeditide.com";
 	if (!(myExt in ko.extensions)) ko.extensions[myExt] = {};
 	if (!('myapp' in ko.extensions[myExt])) ko.extensions[myExt].myapp = {};
